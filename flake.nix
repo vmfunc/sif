f2c8cc71b2f1d28e1fd1af792e11b5f7e628820f
Andrew Gazelka <andrew.gazelka@gmail.com>
2026-01-03 00:25:37 -0800

    chore(nix): modernize flake to use buildGoModule
    - Remove flake-utils dependency (use local forAllSystems helper)
    - Remove gomod2nix dependency (use native buildGoModule)
    - Add overlay export for easy consumption
    - Update nixpkgs to latest unstable
    - Disable tests in nix build (require network access)

--- flake.nix
+++ flake.nix
@@ -1,36 +1,57 @@
 {
-  description = "a blazing-fast pentesting (recon/exploitation) suite";
+  description = "A blazing-fast pentesting (recon/exploitation) suite";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
-
-    gomod2nix = {
-      url = "github:tweag/gomod2nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.utils.follows = "utils";
-    };
   };
 
-  outputs = { self, nixpkgs, utils, gomod2nix }:
-    utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { 
-        inherit system; 
-        overlays = [ gomod2nix.overlays.default ];
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.buildGoModule {
+            pname = "sif";
+            version = "unstable-${self.shortRev or self.dirtyShortRev or "dev"}";
+            src = ./.;
+
+            vendorHash = "sha256-ztKXnOjZS/jMxsRjtF0rIZ3lKv4YjMdZd6oQFRuAtR4=";
+
+            # Tests require network access (httptest)
+            doCheck = false;
+
+            ldflags = [ "-s" "-w" ];
+
+            meta = with pkgs.lib; {
+              description = "Modular pentesting toolkit written in Go";
+              homepage = "https://github.com/vmfunc/sif";
+              license = licenses.bsd3;
+              mainProgram = "sif";
+              maintainers = [ ];
+            };
+          };
+
+          sif = self.packages.${system}.default;
+        });
+
+      overlays.default = final: prev: {
+        sif = self.packages.${final.system}.default;
       };
-      in
-      {
-        packages.default = pkgs.buildGoApplication {
-          pname = "sif";
-          version = "0.1.0";
-          src = ./.;
-          modules = ./gomod2nix.toml;
-        };
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [ 
-            go 
-            gomod2nix.packages.${system}.default
-          ];
-        };
-      });
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [ go gopls ];
+          };
+        });
+    };
 }
